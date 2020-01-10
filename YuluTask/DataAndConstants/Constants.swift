4805c1940930dea0fd5b0db3e9701dd8d2f0fcc8
abhinay varma <abhinay.varma@healthandcare.com>
2020-01-10 15:53:51 +0530

    first commit

--- YuluTask/DataAndConstants/Constants.swift
+++ YuluTask/DataAndConstants/Constants.swift
@@ -7,8 +7,15 @@
 //
 
 import Foundation
+import UIKit
 
 struct Constants {
     static let tableViewCellIdentifier = "mainCell"
+    static let defaultErrorString = "Something went wrong"
+}
+
+struct AppColors {
+    static let leftTableColor = UIColor( red: 153/255, green: 0/255, blue:153/255, alpha: 1.0 ).cgColor
+    static let rightTableColor = UIColor( red: 153/255, green: 153/255, blue:0/255, alpha: 1.0 ).cgColor
     
 }
